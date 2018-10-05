05683c3ab4e0f6ab9bc067d80dd7df7cac8a2e31
Felipe Olivares Fernández <felipeolivares.it@gmail.com>
2018-10-05 16:07:04 -0300

    P4 y P5 listas

--- Lab1/Codigo/P4.m
+++ Lab1/Codigo/P4.m
@@ -1,7 +1,8 @@
 % P4
 clear all
 
-f = @(x) x^3;
-A = [];
+f = @(x) -x^3; % max Vol = - min -Vol
+A = 5;
+b = 97;
 
-x = fmincon(f, [], )
+x = fmincon(f, 1, A, b)
--- Lab1/Codigo/P5.m
+++ Lab1/Codigo/P5.m
@@ -5,12 +5,11 @@ f = @(x) [x(1)^3 + x(2)^3; x(1) - x(2); x(1) + x(2) + 7];
 lb = [-1,-1];
 ub = [0,0];
 
-options = optimoptions('fminimax');
-[x,fval] = fminimax(@myfun,[], [], [], [], [], lb, ub, nonlcon, options);
+[x,fval] = fminimax(@myfun,[0.1,0.1], [], [], [], [], lb, ub);
 
 % Por si acaso
-% function f = myfun(x)
-%     f(1) =  x(1)^3 + x(2)^3;
-%     f(2) = x(1) - x(2);
-%     f(3) = x(1) + x(2) + 7;
-% end
+function f = myfun(x)
+    f(1) =  x(1)^3 + x(2)^3;
+    f(2) = x(1) - x(2);
+    f(3) = x(1) + x(2) + 7;
+end
