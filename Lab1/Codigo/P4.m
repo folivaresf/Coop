56d0d04ac37d07f70398c112aa9c73cfb307ea0b
Felipe Olivares Fernández <felipeolivares.it@gmail.com>
2018-10-05 12:28:06 -0300

    Cambio nombre archivos P4 P5

--- Lab1/Codigo/P4.m
+++ Lab1/Codigo/P4.m
@@ -1,16 +1,7 @@
 % P4
 clear all
 
-f = @(x) [x(1)^3 + x(2)^3; x(1) - x(2); x(1) + x(2) + 7];
-lb = [-1,-1];
-ub = [0,0];
+f = @(x) x^3;
+A = [];
 
-options = optimoptions('fminimax');
-[x,fval] = fminimax(@myfun,[], [], [], [], [], lb, ub, nonlcon, options);
-
-% Por si acaso
-% function f = myfun(x)
-%     f(1) =  x(1)^3 + x(2)^3;
-%     f(2) = x(1) - x(2);
-%     f(3) = x(1) + x(2) + 7;
-% end
+x = fmincon(f, [], )
